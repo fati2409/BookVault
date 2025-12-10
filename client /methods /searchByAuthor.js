3590406b31d5652f0246039e1db70192c967bb09
fati2409 <fatimamalik7891@gmail.com>
2025-12-10 17:02:47 +0500

    Update searchByAuthor.js

--- client /methods /searchByAuthor.js
+++ client /methods /searchByAuthor.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { API_BASE_URL } from '../config.js';
 
-// Task 12: Search by Author using async/await
 export async function searchByAuthor(author) {
     try {
         const url = `${API_BASE_URL}/author/${author}`;
