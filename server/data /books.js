06bafe5b8c651f5dfb1f82fdb4dbb881e503c23f
fati2409 <fatimamalik7891@gmail.com>
2025-12-10 16:49:45 +0500

    Create books.js

---
+++ server/data /books.js
@@ -0,0 +1,54 @@
+// In-memory book database
+export const books = {
+  "978-0-13-468599-1": {
+    isbn: "978-0-13-468599-1",
+    author: "Robert C. Martin",
+    title: "Clean Code: A Handbook of Agile Software Craftsmanship",
+    reviews: []
+  },
+  "978-0-13-235088-4": {
+    isbn: "978-0-13-235088-4",
+    author: "Robert C. Martin",
+    title: "Clean Architecture",
+    reviews: []
+  },
+  "978-0-13-597783-2": {
+    isbn: "978-0-13-597783-2",
+    author: "Martin Fowler",
+    title: "Refactoring: Improving the Design of Existing Code",
+    reviews: []
+  },
+  "978-0-13-475759-9": {
+    isbn: "978-0-13-475759-9",
+    author: "Eric Evans",
+    title: "Domain-Driven Design",
+    reviews: []
+  },
+  "978-0-13-468599-2": {
+    isbn: "978-0-13-468599-2",
+    author: "Martin Fowler",
+    title: "Patterns of Enterprise Application Architecture",
+    reviews: []
+  },
+  "978-0-13-468599-3": {
+    isbn: "978-0-13-468599-3",
+    author: "Andrew Hunt",
+    title: "The Pragmatic Programmer",
+    reviews: []
+  },
+  "978-0-13-468599-4": {
+    isbn: "978-0-13-468599-4",
+    author: "Frederick P. Brooks Jr.",
+    title: "The Mythical Man-Month",
+    reviews: []
+  },
+  "978-0-13-468599-5": {
+    isbn: "978-0-13-468599-5",
+    author: "Steve McConnell",
+    title: "Code Complete",
+    reviews: []
+  }
+};
+
+// In-memory user database
+export const users = {};
