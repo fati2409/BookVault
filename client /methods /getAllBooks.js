4caeee8faeedaba108a188c6d761d196fb8c0862
fati2409 <fatimamalik7891@gmail.com>
2025-12-10 17:00:23 +0500

    Update getAllBooks.js

--- client /methods /getAllBooks.js
+++ client /methods /getAllBooks.js
@@ -1,17 +1,13 @@
 import axios from 'axios';
 import { API_BASE_URL } from '../config.js';
 
-// Task 10: Get all books using async callback function
 export function getAllBooks(callback) {
     const url = `${API_BASE_URL}/books`;
-
     axios.get(url)
         .then(response => {
-            // Success: call callback with null error and data
             callback(null, response.data);
         })
         .catch(error => {
-            // Error: call callback with error and null data
             callback(error, null);
         });
 }
